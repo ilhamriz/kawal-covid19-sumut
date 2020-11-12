6cc5db9d3b41e85aeeaff85d65b04ff5f4da479e
Muhammad Ilham Rizky <milhamrizky7@gmail.com>
2020-11-12 10:09:34 +0700

    fix typo

--- index.js
+++ index.js
@@ -64,6 +64,40 @@ app.get('/api', (req, res) => {
     });
 });
 
+const runAPI = () => {
+    request({
+        url: "http://data.covid19.go.id/public/api/prov.json",
+        json: true
+    }, (err, reponse, body) => {
+        let getSumut = body.list_data.filter((val) => {
+            return val.key == "SUMATERA UTARA";
+        });
+
+        let rawdata = fs.readFileSync('./public/json/saveData.json');
+        let pushdata = JSON.parse(rawdata);
+        let last = pushdata.length - 1;
+        let data_last = pushdata[last];
+        
+        if (getSumut[0].jumlah_kasus != data_last.jumlah_kasus ||
+            getSumut[0].jumlah_sembuh != data_last.jumlah_sembuh ||
+            getSumut[0].jumlah_meninggal != data_last.jumlah_meninggal ||
+            getSumut[0].jumlah_dirawat != data_last.jumlah_dirawat) {
+            
+            let data_pasien = {
+                date: body.last_date,
+                jumlah_kasus: getSumut[0].jumlah_kasus,
+                jumlah_sembuh: getSumut[0].jumlah_sembuh,
+                jumlah_meninggal: getSumut[0].jumlah_meninggal,
+                jumlah_dirawat: getSumut[0].jumlah_dirawat
+            };
+            
+            pushdata.push(data_pasien);
+            fs.writeFileSync('./public/json/saveData.json', JSON.stringify(pushdata, null, 2));
+        }
+    });
+}
+runAPI();
+
 app.get('/api/local', (req, res) => {
     res.send(require('./public/json/data.json'));
 });
